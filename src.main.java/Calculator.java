6e096b59226e7bbddb7e2ba3b7f6146f81ab9a12
sarp çolak <sarpcolak9@hotmail.com>
2020-12-10 22:43:29 +0300

    ADD calculator class and impl

---
+++ src.main.java/Calculator.java
@@ -0,0 +1,61 @@
+
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String args[]) {
+
+      Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter first number:");
+
+        int sayi1= reader.nextInt();
+
+
+
+        System.out.print("Enter second number:");
+
+        int sayi2= reader.nextInt();
+
+
+
+        System.out.print("Enter the calculation mark:");
+
+        char islem = reader.next().charAt(0);
+
+        float sayi3;
+
+
+
+        if(islem=='*'){
+
+            sayi3=sayi1*sayi2;
+
+            System.out.print(sayi3);
+
+        }
+
+        if(islem=='-'){
+
+            sayi3=sayi1-sayi2;
+
+            System.out.print(sayi3);
+
+        }
+
+        if(islem=='+'){
+
+            sayi3=sayi1+sayi2;
+
+            System.out.print(sayi3);
+
+        }
+
+        if(islem=='/'){
+
+            sayi3=(float)sayi1/sayi2;
+
+            System.out.print(sayi3);
+          }
+        }
+      }
